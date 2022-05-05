f2894876921441d48619b14b73ac812bbce45bbd
chris13 <942949735@qq.com>
2022-05-05 10:51:46 +0800

    Add speed up system calls

--- kernel/proc.c
+++ kernel/proc.c
@@ -119,13 +119,22 @@ allocproc(void)
 found:
   p->pid = allocpid();
   p->state = USED;
-
+  
   // Allocate a trapframe page.
   if((p->trapframe = (struct trapframe *)kalloc()) == 0){
     freeproc(p);
     release(&p->lock);
     return 0;
   }
+
+  // Allocate a usyscall page.
+  if((p->mapid = (struct usyscall *)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  // Initialize for mapid
+  p->mapid->pid = p->pid;
 
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
@@ -153,6 +162,11 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
+
+  if(p->mapid)
+    kfree((void*)p->mapid);
+  p->mapid = 0;
+
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
@@ -195,7 +209,15 @@ proc_pagetable(struct proc *p)
     uvmfree(pagetable, 0);
     return 0;
   }
-
+  
+  // map usyscall page(read only), below trapframe.
+  if(mappages(pagetable, USYSCALL, PGSIZE, 
+              (uint64)(p->mapid), PTE_R | PTE_U) < 0) {
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    return 0;
+  }
+  
   return pagetable;
 }
 
@@ -206,6 +228,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0); // +
   uvmfree(pagetable, sz);
 }
 
