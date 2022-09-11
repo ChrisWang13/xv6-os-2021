509f7e7dea92a3c55501dfddfeacb4f0a791449e
ChrisWang13 <chrisqq13@gmail.com>
2022-09-11 18:31:34 +0800

    Add r_fp() to read frame pointer.

--- kernel/riscv.h
+++ kernel/riscv.h
@@ -323,6 +323,15 @@ r_ra()
   return x;
 }
 
+// read frame pointer(s0)
+static inline uint64
+r_fp() 
+{
+  uint64 x;
+  asm volatile("mv %0, s0" : "=r" (x));
+  return x;
+}
+
 // flush the TLB.
 static inline void
 sfence_vma()
